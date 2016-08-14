285da53706cdc67fc09e4d9114f40f1841ec219d
Topi99 <nokoayzack@gmail.com>
2016-08-14 13:47:49 -0500

    No se que sea el error

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^/$', views.Welcome.as_view(), name="welcome"),
+	url(r'^$', views.Welcome.as_view(), name="welcome"),
 ]
