c64836d2a2c198af84850582cf4518e17a5cc0ce
Topi99 <nokoayzack@gmail.com>
2016-08-18 18:09:04 -0500

    Agregamos template base

--- UnMomentow/settings.py
+++ UnMomentow/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'UnMomentow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,3 +107,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
